0b21ac5913831395e08f10231669113324fee3f4
robtweed <rtweed@mgateway.com>
2021-04-09 14:46:34 +0100

    Added performance test examples

---
+++ benchmark_2.js
@@ -0,0 +1,42 @@
+var redis_globals = require('ewd-redis-globals');
+var DocumentStore = require('ewd-document-store');
+
+
+  var params = {
+    database: {
+      type: 'redis'
+    }
+  };
+
+  var db = new redis_globals(params);
+var status = db.open();
+if (status.error) return status;
+  
+
+var documentStore = new DocumentStore(db);
+documentStore.close = db.close.bind(db);
+
+var node = {
+  global: 'cm'
+}
+
+var max = 1000000;
+var d1 = new Date();
+var d1_ms = d1.getTime()
+console.log("d1: " + d1.toISOString());
+var n = 0;
+var value;
+for (n = 1; n <= max; n ++) {
+   node.subscripts = [n];
+   value = db.get(node);
+   //console.log(n + " = " + JSON.stringify(value));
+}
+
+var d2 = new Date();
+var d2_ms = d2.getTime();
+var diff = Math.abs(d1_ms - d2_ms)
+console.log("d2: " + d2.toISOString());
+console.log("records: " + max + " diff: " + diff + " secs: " + (diff / 1000));
+db.close();
+console.log("*** the end ***\n");
+
